9fed3055de7152a3df8feb22a936427c52574f49
garvash <garvash@gmail.com>
2026-03-13 00:38:59 +0900

    feat: implement death timer and respawn logic in Game

--- main.py
+++ main.py
@@ -11,11 +11,26 @@ class Game:
         self.level_map = LevelMap()
         self.player = Player(40, 40, self.level_map)
         self.slime = Slime(40, 40)
+        self.death_timer = 0
         pyxel.run(self.update, self.draw)
 
     def update(self):
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
+        
+        if not self.player.is_alive:
+            self.death_timer += 1
+            if self.death_timer >= 15:
+                self.player.x = self.player.start_x
+                self.player.y = self.player.start_y
+                self.player.is_alive = True
+                self.player.state = "IDLE"
+                self.death_timer = 0
+                # Also reset slime? Usually good idea
+                self.slime.x = self.player.x
+                self.slime.y = self.player.y
+            return
+
         self.player.update(self.slime)
         self.slime.update(self.player.x, self.player.y, self.player.facing_right, self.player.is_fused)
 
